1cc0a2c45fef034c4cc0f89e157fcaedecf0809a
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-06-01 19:18:42 -0500

    implementando personas/models.py y migrando para actualizar los cambios informando al framework

--- Django/destino/src/listaContactos/personas/models.py
+++ Django/destino/src/listaContactos/personas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.TextField(max_length=100)
+    apellido = models.TextField(max_length=100) 
+    edad = models.TextField()
