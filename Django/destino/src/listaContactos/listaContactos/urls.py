bbb6e7a6d7a39fcce4248089a0968a8ad91f4368
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-06-08 22:05:15 -0500

    Django parte 2: creando una nueva vista anotherView para el mensaje <h1>Solo otra página</h1>

--- Django/destino/src/listaContactos/listaContactos/urls.py
+++ Django/destino/src/listaContactos/listaContactos/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from inicio.views import myHomeView
+from inicio.views import anotherView, myHomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", myHomeView, name='Página de inicio'),
-    path('another/', myHomeView, name='Página de inicio')
+    path('another/', anotherView)
 ]
