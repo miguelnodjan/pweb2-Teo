f833edb0f3ce3786413daa4bd30a534d43537499
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-06-08 21:44:26 -0500

    Django parte 2: Creando el valor booleano de si es donador y realizando las debidas modificaciones en el proyecto

--- Django/destino/src/listaContactos/personas/models.py
+++ Django/destino/src/listaContactos/personas/models.py
@@ -5,3 +5,4 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100) 
     edad = models.IntegerField()#(max_digits=3)
+    donador = models.BooleanField()
