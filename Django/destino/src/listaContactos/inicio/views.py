ed8b040f22da7eb30a996add1d7fce6e5dce1887
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-06-08 22:12:58 -0500

    Django parte 2: modificando myHomeView para poder ver  quien realiza la consulta al servidor en la consola del servidor

--- Django/destino/src/listaContactos/inicio/views.py
+++ Django/destino/src/listaContactos/inicio/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def myHomeView(*args, **kwargs):
+def myHomeView(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
     return HttpResponse('<h1> Hola mundo desde Django</h1>')
 
 def anotherView(request):
