4376b370b4626e76b0cc1d5bb1eaa4dc7d3af352
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-06-08 21:56:16 -0500

    Django parte 2: Añadiendo respuesta a la url vacia del puerto con un <h1> de html

--- Django/destino/src/listaContactos/listaContactos/urls.py
+++ Django/destino/src/listaContactos/listaContactos/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from inicio.views import myHomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", myHomeView, name='Página de inicio')
 ]
