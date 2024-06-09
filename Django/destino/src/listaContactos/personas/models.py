e51a598ab715a92d24d83dbf0744341d613320f2
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-06-08 21:10:35 -0500

    Django parte 2: modificando el modelo para que guarde en un mejor formato la información de cada persona, haciendo una migración inclusive

--- Django/destino/src/listaContactos/personas/models.py
+++ Django/destino/src/listaContactos/personas/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombre = models.TextField(max_length=100)
-    apellido = models.TextField(max_length=100) 
-    edad = models.TextField()
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100) 
+    edad = models.IntegerField()#(max_digits=3)
