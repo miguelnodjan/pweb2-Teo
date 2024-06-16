4a8a2fec977bfeea53b36b39e13ebb36c3d15b60
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-06-15 21:27:28 -0500

    creando personasCreate.html y asignandole una url agregar/

--- Django/destino/src/listaContactos/listaContactos/urls.py
+++ Django/destino/src/listaContactos/listaContactos/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inicio.views import anotherView, myHomeView
-from personas.views import personaTestView
+from personas.views import personaTestView, personaCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", myHomeView, name='Página de inicio'),
     path('another/', anotherView, name='otro'),
 
-    path('persona/', personaTestView, name ='otro')
+    path('persona/', personaTestView, name ='testViewPersona'),
+    path('agregar/', personaCreateView, name ='createPersona')
 ]
