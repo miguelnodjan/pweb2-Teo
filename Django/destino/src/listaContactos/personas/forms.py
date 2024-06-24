4dcc7106a7a76dde058f8be2db045c8b9b2b6d7c
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-06-23 20:09:56 -0500

    añadiendo form básico a Personas en forms.py eredando de forms.Form

--- Django/destino/src/listaContactos/personas/forms.py
+++ Django/destino/src/listaContactos/personas/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import Persona
 
-class PersonaForm(forms.ModelForm):
-    class Meta:
-        model = Persona
-        fields = ['nombre', 'apellido', 'edad', 'donador']
+class PersonaForm(forms.Form):
+    nombre = forms.CharField()
+    apellido = forms.CharField()
+    edad = forms.IntegerField()
+    donador = forms.BooleanField()
