d59b708775d9549007887ff15556f17a7771cbd3
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-07-08 20:51:44 -0500

    Agregando un Form para añadir más Hobbies

--- proyectoAngular/my-dream-app/src/app/app.component.ts
+++ proyectoAngular/my-dream-app/src/app/app.component.ts
@@ -29,6 +29,11 @@ export class AppComponent {
   toggleHobbies(){
     this.showHobbies =!this.showHobbies;
   }
+  newHobby(hobby:any){
+    this.hobbies.push(hobby.value);
+    hobby.value="";
+    return false; 
+  }
   
 }
 
